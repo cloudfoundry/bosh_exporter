a76d10d888a334d9c9b5fee30f8fc3b692829f04
Ferran Rodenas <frodenas@gmail.com>
2016-11-05 13:46:00 +0100

    Add regexp filter

---
+++ filters/regexp_filter.go
@@ -0,0 +1,38 @@
+package filters
+
+import (
+	"regexp"
+)
+
+type RegexpFilter struct {
+	reFilters []regexp.Regexp
+}
+
+func NewRegexpFilter(filters []string) (*RegexpFilter, error) {
+	reFilters := []regexp.Regexp{}
+
+	for _, filter := range filters {
+		re, err := regexp.Compile(filter)
+		if err != nil {
+			return nil, err
+		}
+		reFilters = append(reFilters, *re)
+	}
+
+	return &RegexpFilter{reFilters: reFilters}, nil
+}
+
+func (f *RegexpFilter) Enabled(expr string) bool {
+	if len(f.reFilters) == 0 {
+		return true
+	}
+
+	for _, re := range f.reFilters {
+		matched := re.MatchString(expr)
+		if matched {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ filters/regexp_filter_test.go
@@ -0,0 +1,72 @@
+package filters_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/cloudfoundry-community/bosh_exporter/filters"
+)
+
+var _ = Describe("RegexpFilter", func() {
+	var (
+		err     error
+		filters []string
+
+		regexpFilter *RegexpFilter
+	)
+
+	JustBeforeEach(func() {
+		regexpFilter, err = NewRegexpFilter(filters)
+	})
+
+	Describe("New", func() {
+		Context("when filters compile", func() {
+			BeforeEach(func() {
+				filters = []string{"bosh_exporter", "[a-z]+_collector"}
+			})
+
+			It("does not return an error", func() {
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("when filters does not compile", func() {
+			BeforeEach(func() {
+				filters = []string{"[a-(z]+_exporter"}
+			})
+
+			It("returns an error", func() {
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("error parsing regexp: invalid character class range: `a-(`"))
+			})
+		})
+	})
+
+	Describe("Enabled", func() {
+		BeforeEach(func() {
+			filters = []string{"bosh_exporter", "[a-z]+_collector"}
+		})
+
+		Context("when there is a match", func() {
+			It("returns true", func() {
+				Expect(regexpFilter.Enabled("deployments_collector")).To(BeTrue())
+			})
+		})
+
+		Context("when there is not a match", func() {
+			It("returns false", func() {
+				Expect(regexpFilter.Enabled("deployments_exporter")).To(BeFalse())
+			})
+		})
+
+		Context("when there are no filters", func() {
+			BeforeEach(func() {
+				filters = []string{}
+			})
+
+			It("returns true", func() {
+				Expect(regexpFilter.Enabled("deployments_exporter")).To(BeTrue())
+			})
+		})
+	})
+})
